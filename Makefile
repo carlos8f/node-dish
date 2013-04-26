4211db268c8a10779d731d0c73000afafa758aec
Carlos Rodriguez <carlos@s8f.org>
2013-04-26 11:01:12 -0700

    refactor test after change in superagent
    see https://github.com/visionmedia/superagent/commit/0557f72810c96ecd853869001585d996dff51cc5

--- Makefile
+++ Makefile
@@ -1,7 +1,6 @@
 test:
 	@./node_modules/.bin/mocha \
 		--reporter spec \
-		--bail \
 		--timeout 5s \
 		--require test/common.js
 
--- test/basic.js
+++ test/basic.js
@@ -10,44 +10,48 @@ describe('basic test', function () {
 
   it('serve a string', function (done) {
     server.once('request', dish('hello :D'));
-    request.get(baseUrl + '/', function (res) {
-      assert.equal(res.statusCode, 200);
-      assert.equal(res.headers['content-length'], 8);
-      // etag should be sha1
-      assert.equal(res.headers['etag'], '18eddd4897daa9d6096014988cd07f7b688ac46f');
-      // last-modified should be set
-      assert(res.headers['last-modified']);
-      // content-type should be default
-      assert.equal(res.headers['content-type'], 'application/octet-stream');
-      // proper content
-      var data = '';
-      res.on('data', function (chunk) {
-        data += chunk;
+    request
+      .get(baseUrl + '/')
+      .set('Accept-Encoding', 'deflate')
+      .end(function (res) {
+        assert.equal(res.statusCode, 200);
+        assert.equal(res.headers['content-length'], 8);
+        // etag should be sha1
+        assert.equal(res.headers['etag'], '18eddd4897daa9d6096014988cd07f7b688ac46f');
+        // last-modified should be set
+        assert(res.headers['last-modified']);
+        // content-type should be default
+        assert.equal(res.headers['content-type'], 'application/octet-stream');
+        // proper content
+        var data = '';
+        res.on('data', function (chunk) {
+          data += chunk;
+        });
+        res.once('end', function () {
+          assert.equal(data, 'hello :D');
+          done();
+        });
       });
-      res.once('end', function () {
-        assert.equal(data, 'hello :D');
-        done();
-      });
-    });
   });
 
   it('override content-type', function (done) {
     server.once('request', dish('<partytime>carlos</partytime>', {headers: {'Content-Type': 'text/html'}}));
-    request.get(baseUrl + '/', function (res) {
-      assert.equal(res.statusCode, 200);
-      console.log(res.headers);
-      console.log(JSON.stringify(res.text));
-      //assert.equal(res.headers['content-length'], 29);
-      // etag should be sha1
-      assert.equal(res.headers['etag'], '0280e0568eb75bdecb28837d0ac3082140817468');
-      // last-modified should be set
-      assert(res.headers['last-modified']);
-      // content-type should be default
-      assert.equal(res.headers['content-type'], 'text/html');
-      // proper content
-      assert.equal(res.text, '<partytime>carlos</partytime>');
-      done();
-    });
+    request
+      .get(baseUrl + '/')
+      .set('Accept-Encoding', 'deflate')
+      .end(function (res) {
+        assert.equal(res.statusCode, 200);
+        assert.equal(res.headers['content-length'], 29);
+        // etag should be sha1
+        assert.equal(res.headers['etag'], '0280e0568eb75bdecb28837d0ac3082140817468');
+        // last-modified should be set
+        assert(res.headers['last-modified']);
+        // content-type should be default
+        assert.equal(res.headers['content-type'], 'text/html');
+        // proper content
+        assert.equal(res.text, '<partytime>carlos</partytime>');
+        done();
+      });
   });
 
   it('serve a text file', function (done) {
@@ -56,6 +60,7 @@ describe('basic test', function () {
       .get(baseUrl + '/')
       .set('If-Modified-Since', 'Fri, 18 Jan 2013 20:50:19 GMT')
       .set('If-None-Match', 'c01b3f9f12197efbde09ae7870fb39d092a6f6f9')
+      .set('Accept-Encoding', 'deflate')
       .end(function (res) {
         assert.equal(res.statusCode, 200);
         assert.equal(res.headers['content-length'], 12);
@@ -107,37 +112,47 @@ describe('basic test', function () {
 
   it('serve gzipped', function (done) {
     server.on('request', dish.file(path.join(__dirname, 'fixtures', 'hello.txt')));
-    request.get(baseUrl + '/').set('Accept-Encoding', 'gzip, deflate').end(function (res) {
-      assert.equal(res.statusCode, 200);
-      assert.equal(res.headers['content-length'], 32);
-      // etag should be sha1
-      assert.equal(res.headers['etag'], '430ce34d020724ed75a196dfc2ad67c77772d169');
-      eTag = res.headers['etag'];
-      // last-modified should be set
-      assert(res.headers['last-modified']);
-      lastModified = res.headers['last-modified'];
-      // content-type should be text/plain
-      assert.equal(res.headers['content-type'], 'text/plain');
-      // proper content
-      assert.equal(res.text, 'hello world!');
-      done();
-    });
+    request
+      .get(baseUrl + '/')
+      .set('Accept-Encoding', 'gzip, deflate')
+      .end(function (res) {
+        assert.equal(res.statusCode, 200);
+        assert.equal(res.headers['content-length'], 32);
+        // etag should be sha1
+        assert.equal(res.headers['etag'], '430ce34d020724ed75a196dfc2ad67c77772d169');
+        eTag = res.headers['etag'];
+        // last-modified should be set
+        assert(res.headers['last-modified']);
+        lastModified = res.headers['last-modified'];
+        // content-type should be text/plain
+        assert.equal(res.headers['content-type'], 'text/plain');
+        // proper content
+        assert.equal(res.text, 'hello world!');
+        done();
+      });
   });
 
   it('if-none-match', function (done) {
-    request.get(baseUrl + '/').set('If-None-Match', eTag).end(function (res) {
-      assert.equal(res.statusCode, 304);
-      assert(!res.text);
-      done();
-    });
+    request
+      .get(baseUrl + '/')
+      .set('If-None-Match', eTag)
+      .set('Accept-Encoding', 'deflate')
+      .end(function (res) {
+        assert.equal(res.statusCode, 304);
+        assert(!res.text);
+        done();
+      });
   });
 
   it('if-modified-since', function (done) {
-    request.get(baseUrl + '/').set('If-Modified-Since', lastModified).end(function (res) {
-      assert.equal(res.statusCode, 304);
-      assert(!res.text);
-      done();
-    });
+    request
+      .get(baseUrl + '/')
+      .set('If-Modified-Since', lastModified)
+      .end(function (res) {
+        assert.equal(res.statusCode, 304);
+        assert(!res.text);
+        done();
+      });
   });
 
   it('serve 200 again', function (done) {
@@ -145,6 +160,7 @@ describe('basic test', function () {
       .get(baseUrl + '/')
       .set('If-Modified-Since', 'Fri, 18 Jan 2013 20:50:19 GMT')
       .set('If-None-Match', 'c01b3f9f12197efbde09ae7870fb39d092a6f6f9')
+      .set('Accept-Encoding', 'deflate')
       .end(function (res) {
         assert.equal(res.statusCode, 200);
         assert.equal(res.headers['content-length'], 12);
